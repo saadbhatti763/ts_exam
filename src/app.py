e7714da1e21a69c24128835a447bb62e22b35510
saadbhatti763 <saadullahbhatti945@gmail.com>
2024-12-01 11:41:52 +0500

    changed app.py

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ model.fit(X_train, y_train)
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.json
-    prediction = model.predict([data["feature"]])
+    prediction = model.predict([data["features"]])
     return jsonify({"prediction": int(prediction[0])})
 
 if __name__ == "__main__":
